d153cdf2d2eb0e007da458a794980f4794a1f699
Tony Russo <ynotrusso@users.noreply.github.com>
2023-05-05 11:03:24 +0700

    style component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
 function App() {
-  return <div>R</div>
+  const ratingOptions = [1, 2, 3, 4, 5]
+  return (
+    <div className="layout">
+      <div className="step-1">
+        <h3>Please rate your experience</h3>
+        <div className="rating">
+          {ratingOptions.map((r, i) => (
+            <button>{r}</button>
+          ))}
+        </div>
+      </div>
+      {/* <div className="step-2">
+        <h3>Thank you for your feedback!</h3>
+      </div> */}
+    </div>
+  )
 }
 
 export default App
